d074a2e47268130a201cae89efe35345213b3488
Ruslan Kornev <oganer@gmail.com>
2013-11-19 22:42:37 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FayeCapybara::Application.config.secret_key_base = '994c2e4f552399199e3f3931b9d776f598e591b4987de283368f6350db154b4cbbb45aaabfa4a87cde98a4df19dd4fcda55043d19dc5584fc742784c535ad6a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FayeCapybara::Application.config.session_store :cookie_store, key: '_faye_capybara_session'
